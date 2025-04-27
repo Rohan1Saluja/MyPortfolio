9d39ef3b6e0b69464674b572db3fabe2fc11b03b
Rohan Saluja <saluja@Rohans-MacBook-Pro.local>
2025-04-27 22:20:30 +0530

    Add community page

--- src/api/contact.ts
+++ src/api/contact.ts
@@ -1,6 +1,6 @@
 import baseApi from "./baseApi";
 
 export const sendMail = async (payload: any) => {
-  const response = baseApi.post("/api/contact", payload);
+  const response = baseApi.post("/contact", payload);
   return response;
 };
--- src/pages/Home/Sections/Folio.tsx
+++ src/pages/Home/Sections/Folio.tsx
@@ -3,7 +3,15 @@ import { projects } from "../utils";
 const Folio: React.FC = () => {
   return (
     <section id="folio" className="my-10 px-4">
-      <h2 className="text-3xl font-semibold text-center mb-10">My Work</h2>
+      <h2 className="text-3xl font-semibold text-center mb-8">
+        Crafting Solutions: A Glimpse into My Journey
+      </h2>
+      <p className="text-center text-lg text-gray-300 mb-6 max-w-[85%] md:max-w-2/3 mx-auto animate-fade-in">
+        From building dynamic UIs and AI-powered interfaces at Recurrent
+        Software to optimizing logistics and enhancing customer satisfaction at
+        Repairable, here are some of the projects that showcase my expertise and
+        passion for software development.
+      </p>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 gap-6 justify-center md:max-w-2xl mx-auto">
         {projects.map((project, index) => (
           <div
